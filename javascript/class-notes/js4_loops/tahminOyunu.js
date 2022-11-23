828b50b4a750b56d4fd2d67a3a4c599843dcf6bd
Erhan <erhantezer68@gmail.com>
2022-11-24 01:46:29 +0300

    updated

--- javascript/class-notes/js4_loops/tahminOyunu.js
+++
@@ -1,4 +0,0 @@
-//? ODEV2: Tahmin Oyunu
-//* Program 0*100 arasında rasgele bir sayı tutacak ve kullanıcının bu sayıyı 5 kere(hak) de bilmesini isteyecektir. Her yanlışta hakkını bir düşürecek ve ARTTIR/AZALT diyerek kullanıcıyı yönlendirecektir.Sonuç olarak kullanıcının hakkı 0 olursa "Üzgünüz bilemediniz" eğer bildi ise "Tebrikler ... denemede bildiniz" yazacaktır.
-
-console.log('******** OYUN ********');
