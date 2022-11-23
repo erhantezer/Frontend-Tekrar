44a0c9ba2d396bb3259013cbd7630553f6881928
Erhan <erhantezer68@gmail.com>
2022-11-23 20:21:52 +0300

    switch

--- javascript/class-notes/js03_conditions/app.js
+++ javascript/class-notes/js03_conditions/app.js
@@ -1,4 +1,4 @@
-console.log('**********\nSession 3 - Conditionals\n**********');
+//? console.log('**********\nSession 3 - Conditionals\n**********');
 // ! if statement example
 // check if the is positive
 
@@ -17,19 +17,65 @@ console.log('**********\nSession 3 - Conditionals\n**********');
 
 // suggest size for men as XS S M L XL XXL on chest size
 // const chest = +prompt("Omuz ölcünüzü giriniz:");
-const size = +prompt("Beden ölçünüzü giriniz:");
+// const size = +prompt("Beden ölçünüzü giriniz:");
 
-if(size<32) {
-console.log("Wrong size");
-}
-else if (size>=32 && size<=35) {
-console.log("XS");
-}
-else if(size>=36 && size<=38){
-console.log("S");
-}
-else{
-console.log(` Beden numarası ${size} gelecek`);
-}
+// if(size<32) {
+// console.log("Wrong size");
+// }
+// else if (size>=32 && size<=35) {
+// console.log("XS");
+// }
+// else if(size>=36 && size<=38){
+// console.log("S");
+// }
+// else{
+// console.log(` Beden numarası ${size} gelecek`);
+// }
 
+
+// ! example 2 three number comparison
+// Write a JavaScript program that accept three integers and display the greatest.
+
+// const n1 = Number(prompt("Enter n1 Number:"));
+// const n2 = Number(prompt("Enter n2 Number:"));
+// const n3 = Number(prompt("Enter n3 Number:"));
+
+// if(n1>n2 && n1>n3){
+//     console.log(`n1= ${n1} En büyük sayıdır`);
+// }else if(n2>n3 && n2>n1){
+//     console.log(`n2= ${n2} En büyük sayıdır`);
+// }else{
+//     console.log(`n3= ${n3} en büyük sayıdır`)
+// }
+
+
+// ! ternary examples
+// console.log('----------\nternary if statements\n----------');
+
+// program to check pass or fail
+
+// let grade = +prompt("Enter your score:)");
+
+// const not = grade>50 ? "Pass" : "Fail";
+// console.log(not);
+
+
+// let num = +prompt("Enter your number:)");
+// const result = num>=0 ? (num==0 ? "Sıfır" : "Positif") : "Negatif";
+// console.log(result)
+
+
+// ! example-4 canDrive
+// let canDrive = false;
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+// const isHandicapped = false;
+// const usingRegularCar = true;
+
+// const result =
+//   hasDriversLicense && hasGoodVision ? !isHandicapped ? 'can drive' : !usingRegularCar ? 'can drive' : 'cannot drive' : 'cannot drive';
+// console.log(result);
+
+
+//? console.log('----------\nswitch case statements\n----------');
 
