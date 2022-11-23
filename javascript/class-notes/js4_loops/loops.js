7e812151de350c3fcba5494438d06fc6b85c3356
Erhan <erhantezer68@gmail.com>
2022-11-24 01:04:21 +0300

    updated

--- javascript/class-notes/js4_loops/loops.js
+++ javascript/class-notes/js4_loops/loops.js
@@ -62,6 +62,65 @@ console.log('****** LOOPS *******');
 // }
 // console.log(`${sayi} ${asalMı(sayi)}`);
 
+
 //! *********************** WHILE ***************************
 
+// let j = 1;
+// while (j <= 5) {
+//   console.log('Merhaba-', j);
+//   j++;
+// }
+// console.log('Bitti');
+
+
+// let i = 1;
+// do {
+//     console.log("Merhaba", i)
+//     i++
+// }while(i<5);
+
+
+// let not1 = +prompt('Lütfen notunuzu giriniz (0-100):')
+
+// while(not1<0 || not1>100){
+//     alert('Girdiğiniz not 0-100 arasında olmalıdır')
+//     not1=+prompt('Lütfen notunuzu giriniz (0-100):')
+// }
+
+// console.log("Not", not1);
+
+
 //! ********************** DO-WHILE *************************
+
+// let not2;
+
+// do {
+//   not2 = prompt('Lütfen notunuzu giriniz (0-100):');
+//   if (not2 < 0 || not2 > 100) {
+//     alert('Girdiğiniz not 0-100 arasında olmalıdır');
+//  }
+// } while (not2 < 0 || not2 > 100);
+
+// console.log('Girdiğiniz not', not2);
+
+//? ODEV1: klavyeden Q karakteri girilene kadar not girişi yapan bir programı döngü kullanarak yazınız.
+
+// let charr;
+// do{
+//  charr = prompt("Bir karakter giriniz")
+// //  if(charr!="q" && charr != "Q"){
+// //     alert("uygun karakteri giriniz")
+// // }
+// }while(charr!="q" && charr != "Q");
+
+// console.log("Karakter: ", charr)
+
+
+
+// let not3;
+
+// while (not3 != 'q') {
+//   not3 = prompt('Lütfen not giriniz veya Q ya basarak çıkınız!').toLowerCase();
+// }
+
+// console.log('Q ya basarak çıkış yaptınız !!!');
