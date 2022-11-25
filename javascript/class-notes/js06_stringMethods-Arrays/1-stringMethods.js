2113e4dc88b771ae114cfe8fed1ab92ddfaac6f7
Erhan <erhantezer68@gmail.com>
2022-11-26 00:02:28 +0300

    ödev finished

--- javascript/class-notes/js06_stringMethods-Arrays/1-stringMethods.js
+++ javascript/class-notes/js06_stringMethods-Arrays/1-stringMethods.js
@@ -123,3 +123,9 @@ console.log(copy);//! ['G', 'e', 'l', ' ', 'g', 'ü', 'n', 'd', 'ü', 'z', 'l', 
 //* ----------------------------------------------------------
 // https://www.youtube.com/watch?v=b7vfp5G4brE
 // https://youtu.be/b7vfp5G4brE
+
+const youtube = ("https://www.youtube.com/watch?v=b7vfp5G4brE");
+const splitedLink = youtube.split("=");
+console.log(splitedLink);
+const shortLink = `https://youtu.be/${splitedLink[1]}`;
+console.log(shortLink)
