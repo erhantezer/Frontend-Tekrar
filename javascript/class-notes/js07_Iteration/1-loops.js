b30fbc98a1f30392ad5abb18c976311ca76c85b4
Erhan <erhantezer68@gmail.com>
2022-11-27 01:13:41 +0300

    updated

--- javascript/class-notes/js07_Iteration/1-loops.js
+++ javascript/class-notes/js07_Iteration/1-loops.js
@@ -24,3 +24,11 @@ console.log(isToplami());
 //* ======================================================
 //*                   FOR-IN LOOP
 //* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Ogrenciler dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrncileri aramamızı sağlayacak ve aranılan ogrenciden
+//* ne kadar sayida bulunuldugunu  ana programa döndürecek bir
+//*  fonksiyonu yazınız. Eğer aranılan isimde bir ogrenci yok ise
+//* fonksiyon aranılan ogrenci bulunamdı dondurulmelidir.
+//*--------------------------------------------------------
