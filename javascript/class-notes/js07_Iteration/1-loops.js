607a989a202290fa94e206599cb2a6671604acf2
Erhan <erhantezer68@gmail.com>
2022-11-27 01:11:45 +0300

    updated

--- javascript/class-notes/js07_Iteration/1-loops.js
+++ javascript/class-notes/js07_Iteration/1-loops.js
@@ -3,3 +3,24 @@
 //* ======================================================
 
 console.log('******* LOOPS IN ARRAYS ******');
+
+const islemler = [-3000, 4500, -2200, -6000, 9000];
+
+const isToplami = () => {
+    let yatirma = 0;
+    let cekme = 0;
+    for (let i=0; i < islemler.length; i++) {
+        if (islemler[i] >= 0) {
+            yatirma +=islemler[i]
+        } else {
+            cekme += islemler[i]
+        }
+    };
+    return `Toplam yatirma: ${yatirma}, Toplam Cekme:${-cekme}`;
+};
+
+console.log(isToplami());
+
+//* ======================================================
+//*                   FOR-IN LOOP
+//* ======================================================
