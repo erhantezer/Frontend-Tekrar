ef84fb304d8a3036fed0c75c0d01d5c9d2875430
Erhan <erhantezer68@gmail.com>
2022-11-27 08:09:24 +0300

    for in and for of finishid

--- javascript/class-notes/js07_Iteration/1-loops.js
+++ javascript/class-notes/js07_Iteration/1-loops.js
@@ -32,3 +32,56 @@ console.log(isToplami());
 //*  fonksiyonu yazınız. Eğer aranılan isimde bir ogrenci yok ise
 //* fonksiyon aranılan ogrenci bulunamdı dondurulmelidir.
 //*--------------------------------------------------------
+
+const ogrenciler = [
+    'ahmet',
+    'mehmet',
+    'ismet',
+    'saffet',
+    'ahmet',
+    'nur',
+    'can',
+    'nur',
+    'mehmet',
+    'cem',
+  ];
+
+const ogrenciBul = function (isim) {
+    let sayac =0;
+    for(let i in ogrenciler) {
+        // ogrenciler[i] === isim ? sayac++ : null ;
+        ogrenciler[i] === isim && sayac++;
+    }
+
+    return sayac === 0 ? `${isim} bulunamadı`: `${isim}, ${sayac} adet bulundu`
+}
+
+// let ogrIsim = prompt("Ogrenci adini giriniz: ").toLocaleLowerCase();
+// console.log(ogrenciBul(ogrIsim));
+
+
+//* ======================================================
+//*                   FOR-OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini
+//* kapsayacak sekilde guncellenmis halidir. Bu dongude dizi
+//* icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+
+const ogrenciBulForOf = function (isim) {
+    let sayac = 0;
+    for (let ogrenci of ogrenciler) {
+        ogrenci === isim ? sayac++ : null; //! Ternary
+        //! Short-circuit yöntemi: && => koşul doğru ise ifadeyi çalıştır.
+        // ogrenci === isim && sayac++;
+
+        //! Short-circuit yöntemi: || => koşul yanlış ise ifadeyi çalıştır.
+        // ogrenci === isim || sayac--; //! Short-circuit yöntemi
+
+        return !sayac ? `${isim} bulunamadi` : `${isim}, ${sayac} adet bulundu`;
+        // return sayac ? `${isim}, ${sayac} adet bulundu` : `${isim} bulunamadi`;
+    }
+};
+
+// let ogrName = prompt('Ogrenci adini giriniz:').toLocaleLowerCase();
+// console.log(ogrenciBulForOf(ogrName));
