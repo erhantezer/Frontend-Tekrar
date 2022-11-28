60be184aa8bf314bad29d2190da2e4b77eb9fd4e
Erhan <erhantezer68@gmail.com>
2022-11-29 01:07:45 +0300

    iteration started

--- javascript/class-notes/js07_Iteration/2-iteration.js
+++ javascript/class-notes/js07_Iteration/2-iteration.js
@@ -4,3 +4,16 @@
 //* ======================================================
 
 console.log('****** ITERATION **********');
+
+//*Dizideki herbir fiyati konsola bastiriniz.
+
+const fiyatlar = [100, 250, 50, 89];
+
+fiyatlar.forEach((x) => console.log(x));
+
+// function yazdir(value) {
+//     console.log(value)
+// }
+// fiyatlar.forEach(yazdir)
+
+console.log('***************************');
