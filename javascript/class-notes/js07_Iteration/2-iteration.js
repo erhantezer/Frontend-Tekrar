3dedd943bf7adb556300da9bb1ca8610e799c0d8
Erhan <erhantezer68@gmail.com>
2022-11-29 14:02:45 +0300

    map finished

--- javascript/class-notes/js07_Iteration/2-iteration.js
+++ javascript/class-notes/js07_Iteration/2-iteration.js
@@ -46,4 +46,36 @@ const fiyatlarx = [100, 250, 50, 89];
 fiyatlarx.forEach((fiyat, indis, dizi) => {
     dizi[indis] = Math.round(fiyat * 1.1);
 })
-console.log(fiyatlarx)
+console.log(fiyatlarx);
+
+//* ======================================================
+//*                       MAP METHOD
+//* ======================================================
+
+//*-------------- ÖRNEK -------------------
+//* isimler dizisin içerisindeki her ismi büyük harf olarak
+//* yeni bir diziye saklayalım.
+
+const isimler = [
+    'Ahmet',
+    'mehmet',
+    'ismet',
+    'SAFFET',
+    'Can',
+    'Canan',
+    'Cavidan',
+];
+console.log(isimler);
+console.log(isimler.map((isim) => isim.toLocaleUpperCase()));
+
+const kücükIsimler = isimler.map((isim) => isim.toLocaleLowerCase());
+console.log(kücükIsimler);
+
+//*-------------- ÖRNEK -------------------
+
+const buyukIsimler = isimler.map((isim, indis, dizi) => {
+    dizi[indis] = isim.toLocaleLowerCase(); //! Orjinal diziyi değiştirdik.
+    return isim.toLocaleUpperCase();
+})
+console.log(buyukIsimler);
+console.log(isimler);
