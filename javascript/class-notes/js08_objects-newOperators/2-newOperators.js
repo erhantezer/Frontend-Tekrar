8c4b229670a488170de54360bc2b2127af75d1ce
Erhan <erhantezer68@gmail.com>
2022-11-29 22:31:11 +0300

    updated

--- javascript/class-notes/js08_objects-newOperators/2-newOperators.js
+++ javascript/class-notes/js08_objects-newOperators/2-newOperators.js
@@ -18,3 +18,26 @@ const {name, model, engine} = car;
 console.log(name, model, engine);
 
 // //* EXAMPLE: NESTED
+const cars = {
+    car1: {
+        name: 'BMW',
+        model: 1990,
+        engine: 1.6,
+    },
+    car2: {
+        name: 'Mercedes',
+        model: 2022,
+        engine: 2.0,
+    },
+};
+
+const {car1, car2} = cars;
+console.log(car1, car2);
+
+const {name:name1, model:model1, engine:engine1} = car1;
+const {name:name2, model:model2, engine:engine2} = car2;
+console.log(name1, model1, engine1);
+console.log(name2, model2, engine2);
+
+
+
