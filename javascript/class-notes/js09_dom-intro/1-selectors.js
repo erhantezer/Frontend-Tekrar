f92a0ef94e959da693195c15edd5d553c92ef633
Erhan <erhantezer68@gmail.com>
2022-12-01 12:41:58 +0300

    updated

--- javascript/class-notes/js09_dom-intro/1-selectors.js
+++ javascript/class-notes/js09_dom-intro/1-selectors.js
@@ -3,3 +3,18 @@
 //*===========================================
 
 console.log('***** SELECTORS *****');
+
+//*===========================================
+//*            GETELEMENTBYID()
+//*===========================================
+
+//* EXAMPLE-1 (Paragraph Style)
+//*--------------------------------------------
+
+const header = document.getElementById("header");
+console.log(header);
+
+header.style.color = "red";
+
+//* EXAMPLE-2 (Button Style)
+//*-------------------------------------------
