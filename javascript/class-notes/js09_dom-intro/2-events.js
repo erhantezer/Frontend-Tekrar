f45ff66536a6c26f30b7481bbfd1a453f5aa1ff1
Erhan <erhantezer68@gmail.com>
2022-12-01 14:42:15 +0300

    updated js

--- javascript/class-notes/js09_dom-intro/2-events.js
+++ javascript/class-notes/js09_dom-intro/2-events.js
@@ -3,3 +3,26 @@
 //* ======================================================
 
 console.log('****** EVENTS *******');
+
+//* EXAMPLE-1 (Mouse Over, Mouse Out )
+//* -------------------------------------------------------
+
+//? METHOD-3 (ONLY JAVASCRIPT)
+//?--------------------------------------------------------
+// document.getElementById("header").onclick = function () {
+//     document.getElementById("header").style.color ="red";
+// }
+
+// const header =document.getElementById("header");
+const header =document.querySelector("#header");
+
+header.onmouseover = () => {
+    header.style.color = "pink";
+}
+header.onmouseout = () => {
+    header.style.color = "black";
+}
+
+
+//? METHOD-4 (ONLY JAVASCRIPT - addEventListener())
+//?-------------------------------------------------------
