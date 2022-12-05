50f5757a7415d278294d9167bddc1d50cc5f5ffd
Erhan <erhantezer68@gmail.com>
2022-12-05 20:32:58 +0300

    updated

--- javascript/class-notes/js12_ProjectWeek_todo/app.js
+++ javascript/class-notes/js12_ProjectWeek_todo/app.js
@@ -3,3 +3,6 @@
 //* ======================================================
 
 //? Selectors
+const btn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = documnet.getElementById("todo-ul");
