d8888618d82559851cf9d72bcd76ba5db4b509ef
Erhan <erhantezer68@gmail.com>
2022-12-05 20:45:17 +0300

    updated

--- javascript/class-notes/js12_ProjectWeek_todo/app.js
+++ javascript/class-notes/js12_ProjectWeek_todo/app.js
@@ -5,4 +5,14 @@
 //? Selectors
 const btn = document.getElementById("todo-button");
 const todoInput = document.getElementById("todo-input");
-const todoUl = documnet.getElementById("todo-ul");
+const todoUl = document.getElementById("todo-ul");
+
+//! Baslangicta input aktif olsun
+// window.onload = function () {
+//     todoInput.focus();
+// }
+window.addEventListener("load", () =>{
+    todoInput.focus()
+})
+
+//! Add Buton Event'inin tanimanmasi
