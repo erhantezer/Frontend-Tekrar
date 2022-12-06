7f9ed6b30bea9a257d084bae35355abbd575bc10
Erhan <erhantezer68@gmail.com>
2022-12-06 17:41:27 +0300

    updated

--- javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
+++ javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
@@ -142,3 +142,20 @@ const calculate = () => {
     previousOperand = '';
     operation = '';
 };
+
+
+function switchMode(){
+    let moon=document.getElementById("moon");
+    if(moon.className=="moon"){
+        moon.className="sun";
+        document.body.style.backgroundColor="#141D26";
+
+        document.body.style.color="#fff";
+    }
+    else{
+        moon.className="moon";
+        document.body.style.backgroundColor="#fff";
+        document.body.style.color="#000";
+        
+    }
+}
