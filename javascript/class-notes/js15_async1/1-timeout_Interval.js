c8a70678f389430b8241bb44291ce398cf7d79d4
Erhan <erhantezer68@gmail.com>
2022-12-06 21:38:10 +0300

    async1

--- javascript/class-notes/js15_async1/1-timeout_Interval.js
+++ javascript/class-notes/js15_async1/1-timeout_Interval.js
@@ -18,20 +18,81 @@
 
 //* Senkron
 //* ---------------------------------------------------------------
-const bekle = (ms) => {
-    //! blocking-code
-    const start =new Date().getTime();
-    // console.log(start);
-    while(new Date().getTime() < start + ms){}
-    //! wihile ile 3sn beklemesini sağlıyoruz
-}
+// const bekle = (ms) => {
+//     //! blocking-code
+//     const start =new Date().getTime();
+//     console.log(start);
+//     while(new Date().getTime() < start + ms){}
+//     //! wihile ile 3sn beklemesini sağlıyoruz
+// }
 
-console.log("hello");
-alert("ITF"); //! blocking-code
-console.time("timer")
-bekle(3000)
-console.timeEnd("timer")
-console.log('FS11');
+// console.log("hello");
+// alert("ITF"); //! blocking-code
+// console.time("timer")
+// bekle(3000)
+// console.timeEnd("timer")
+// console.log('FS11');
 
 //* Asenkron (setTimeout)
 //* ---------------------------------------------------------------
+//! macro task kuyrugu
+// setTimeout(() => {
+    //! non-blocking code
+//     console.log("hello Erhan");
+// },3000);
+
+// //! micro task kuyrugu
+// console.log("hello Kazım");
+// console.log("Merhaba");
+
+// setTimeout(() => {
+    //! non-blocking code
+//     console.log("Yavrum nerdesin")
+// },1000);
+
+
+//* Asenkron (setInterval, clearInterval)
+//*----------------------------------------------------------
+// console.log("Counter Started");
+// let counter = 1;
+
+// const sec1 = setInterval(() => {
+//     console.log(counter++);
+//     if(counter > 10){
+//         clearInterval(sec1)
+//     }
+    
+// }, 1000);
+
+// console.log("Counter Finished");
+
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakta bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+setTimeout(() =>{
+    console.log("John: Hi");
+    setTimeout(() =>{
+        console.log("Sarah:Hello");
+        setTimeout(() =>{
+            console.log("John: How are you?");
+            setTimeout(() =>{
+                console.log("Sarah: I'm fine and you?");
+                setTimeout(() =>{
+                    console.log("John: Thanks I'm good");
+                },2000);
+            },2000);
+        },2000);
+    },2000);
+},2000);
+
+//? COZUMLER:
+//?----------------------------------------------------------------
+//* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+//? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+//* 2- Promise,
+//! 3- Fetch API (Promise'in basitlestirilmis hali),
+//! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
