9dc7d191841c268a545a7ad0d2cc51fce9a9b69c
Erhan <erhantezer68@gmail.com>
2022-12-06 15:15:30 +0300

    updated

--- javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
+++ javascript/class-notes/js13_ProjectWeek_IOSCalc/app.js
@@ -7,3 +7,20 @@ const currDisp = document.querySelector('.current-display');
 
 //?Button container (capturing metodu ile)
 const btnContainer = document.querySelector('.buttons-container');
+
+
+
+//! capturing uygularsak 
+//? Butonlari tasiyan container icin event tanimlamasi
+btnContainer.addEventListener("click", (e) => {
+    if(e.target.classList.contains("num")) {
+        // currDisp.innerText=e.target.innerText
+        appendNumber(e.target.innerText)
+    }
+    // console.log(e.target.classList.contains("num"));
+    // console.log(e.target.innerText)
+
+    if (e.target.classList.contains("operator")) {
+        
+    }
+})
