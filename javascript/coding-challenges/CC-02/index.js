11821beb66af9194f1b4fc83ac7af30afd742c92
Erhan <erhantezer68@gmail.com>
2022-12-09 00:21:47 +0300

    fib number

--- javascript/coding-challenges/CC-02/index.js
+++ javascript/coding-challenges/CC-02/index.js
@@ -1,3 +1,19 @@
 console.log("*********** Fibonacci Number ************");
 
+let fib = +prompt("Kaçıncı fib sayısına ulaşmak istersiniz");
 
+let seri = [1];
+function fibonacci(num) {
+    
+    for(let i= 1; i < 10; i++){
+        if(i === 1){
+            seri.push(i)
+        }else {
+            seri.push(seri[i-2] + seri[i-1])
+        }
+        
+    }
+    return seri[num-1]
+}
+
+console.log(fibonacci(fib));
