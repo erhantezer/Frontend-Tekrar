754269949b77ff42d3fc61bde31a74e7edc06f25
Erhan <erhantezer68@gmail.com>
2022-12-11 13:24:20 +0300

    selected dom

---
+++ javascript/class-notes/js17_weather-app/weather.js
@@ -0,0 +1,6 @@
+console.log("********* WEATHER APP **********");
+
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".top-banner input")
+const msg = document.querySelector("span.msg")
+const list = document.querySelector(".ajax-section .cities");
