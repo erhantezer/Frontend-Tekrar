58a6bca8ecf016f35ceed62b32c2cda8abc9d2be
Erhan <erhantezer68@gmail.com>
2022-12-15 00:34:17 +0300

    updated

--- react/class-notes/rc05_events_useStateHook/solution/src/App.js
+++ react/class-notes/rc05_events_useStateHook/solution/src/App.js
@@ -1,10 +1,12 @@
 import Events from "./components/1-event-intro/Events";
+import Counter from "./components/2-classComponents/Counter";
 
 
 function App() {
   return (
     <div className="App">
       <Events/>
+      <Counter/>
     </div>
   );
 }
