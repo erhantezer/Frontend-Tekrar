a06fc6d4fd44028ff4d9f30d34d6feb4c8982639
Erhan <erhantezer68@gmail.com>
2022-12-14 21:39:24 +0300

    tour places finished

--- react/class-notes/rc04_tour-places-rp-01/src/components/Main/Main.jsx
+++ react/class-notes/rc04_tour-places-rp-01/src/components/Main/Main.jsx
@@ -1,8 +1,20 @@
+import {data} from "../../helpers/data"
+import Card from "./Card";
+import "./Main.scss";
 
+//! props={item} yada {...item} spread ile verileri açıp gönderebiliriz
 
 const Main = () => {
+    console.log(data);
     return (
-        <div>Main</div>
+        <div className="card-container">
+            {data?.map((item, index) => {
+            
+                return(
+                    <Card key={index} {...item}/>
+                )
+            })}
+        </div>
     )
 }
 
