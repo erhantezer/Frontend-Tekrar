442b7c20c81c213a9683d10ddfc75456b09e33ba
Erhan <erhantezer68@gmail.com>
2022-12-15 01:19:07 +0300

    updated

--- react/class-notes/rc05_events_useStateHook/solution/src/App.js
+++ react/class-notes/rc05_events_useStateHook/solution/src/App.js
@@ -1,12 +1,13 @@
 import Events from "./components/1-event-intro/Events";
 import Counter from "./components/2-classComponents/Counter";
-
+import UseStateExample from "./components/3-useStateHook/UseStateExample";
 
 function App() {
   return (
     <div className="App">
       <Events/>
       <Counter/>
+      <UseStateExample/>
     </div>
   );
 }
--- react/class-notes/rc05_events_useStateHook/solution/src/components/3-useStateHook/useStateExample.jsx
+++ react/class-notes/rc05_events_useStateHook/solution/src/components/3-useStateHook/useStateExample.jsx
@@ -18,12 +18,67 @@
 //?    https://reactjs.org/docs/hooks-rules.html
 //* =====================================================================
 
+import { useState } from "react"
 
 
-const useStateExample = () => {
-  return (
-    <div>useStateExample</div>
-  )
+const UseStateExample = () => {
+ //* useState en cok kullanilan Hook'tur.
+ //* Bir state'in degisken, dizi ve obje ile kullanilabilmesine olanak saglar
+ //? useState hook'u 2 deger dondurur.
+ //?  1.si state degiskenidir.
+ //?  2.si ise state'i degistirmeye izin veren bir setter metodudur.
+ //? useState parametre olarak state'in ilk degerini alir.
+
+const [count, setCount] = useState(0); //! array destruc.
+
+const [info, setInfo] = useState({
+    name:"Ahmet YILMAZ",
+    email: "ay@gmail.com",
+    age:32,
+})
+
+const inc = () => {
+    setCount(count+1)
 }
 
-export default useStateExample
+const incAge = () => {
+// setInfo(info.age + 1);
+// console.log(info.age);
+// setInfo({ name: 'mehmet yilmaz', email: 'my@gmail.com', age: 44 });
+//? key:Value
+setInfo({...info, age:info.age + 1}); //! açıp(spread) istediğimizi kullandık
+}
+
+    return (
+        <div className="container text-center mt-4">
+        <section>
+            <h1 className="display-4 text-danger m-4">useState COUNT: {count}</h1>
+            <button onClick={inc} className="btn btn-success">
+                INC
+            </button>
+            <button onClick={() => setCount(count-1)} className="btn btn-dark ms-2">
+                DEC
+            </button>
+            <button onClick={() => setCount(0)} className="btn btn-info ms-2">
+                CLR
+            </button>
+        </section>
+
+        <section>
+            <h1 className="display-4 text-danger mt-4">USESTAE OBJECT</h1>
+            <h2>{info.name}</h2>
+            <h2>{info.email}</h2>
+            <h2>{info.age}</h2>
+            <button onClick={incAge} className="btn btn-secondary">
+            Inc Age
+            </button>
+            <button onClick={() => setInfo({...info, age:info.age-1})} className="btn btn-primary ms-2">
+                Dec Age
+            </button>
+        </section>
+
+        </div>
+    )
+}
+
+export default UseStateExample
