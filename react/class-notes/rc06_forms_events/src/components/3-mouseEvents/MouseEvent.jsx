29e345cce7affec762a4e0de450c41a2b2e0efe8
Erhan <erhantezer68@gmail.com>
2022-12-15 15:17:26 +0300

    mouse event finished

--- react/class-notes/rc06_forms_events/src/components/3-mouseEvents/MouseEvent.jsx
+++ react/class-notes/rc06_forms_events/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,9 +1,65 @@
-
+import { useState } from 'react';
+import { FaReact } from 'react-icons/fa';
+import { FaEvernote } from 'react-icons/fa';
 
 const MouseEvent = () => {
+  const [visible, setVisible] = useState(false);
+  const [toggle, setToggle] = useState(false)
+    
+  const handleToggle = (e) => {
+    setToggle(!toggle)
+    toggle  ? (e.target.className = 'bg-danger text-light w-50 p-4 mt-4')
+            : (e.target.className = "bg-success text-light w-50 p-4 mt-4")
+  }
+
+
+
+const [coordX, setCoordX] = useState();
+const [coordY, setCoordY] = useState();
+
+const handleMouseMove = (e) => {
+   //! Goreceli koordinatlar
+    setCoordX(e.nativeEvent.offsetX);
+    setCoordY(e.nativeEvent.offsetY);
+
+    //! Sayfaya göre koordinatlar
+    // setCoordX(e.pageX);
+    // setCoordY(e.pageY)
+}
   return (
-    <div>
-      
+    <div className="container text-center d-flex flex-column align-items-center mt-4">
+      <h2 className="text-danger">MOUSE EVENTS</h2>
+
+
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseOver={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+      >
+        todo item 1 <FaReact /> {visible && <span>This is a hint</span>}
+      </div>
+
+
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleToggle}
+      >
+        todo item 2 <FaEvernote />
+      </div>
+
+
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseMove={handleMouseMove}>
+        todo item 3 <FaReact />
+      </div>
+
+      <p>X and Y</p>
+      <p className="text-danger fw-bold">{coordX} {coordY}</p>
+
     </div>
   )
 }
