0c54071c581be6aed22f374b67e8fe6b50f26800
Erhan <erhantezer68@gmail.com>
2022-12-19 00:16:26 +0300

    updated

--- react/class-notes/rc09-10_tutorials-example/src/pages/Home.jsx
+++ react/class-notes/rc09-10_tutorials-example/src/pages/Home.jsx
@@ -1,6 +1,25 @@
-
+import { useEffect, useState } from "react"
+import axios from "axios";
 
 const Home = () => {
+    const [tutorials, setTutorials] = useState();
+
+    const url = 'https://639f7c797aaf11ceb89ba52c.mockapi.io/tutorials';
+
+    const getTutorials = async () => {
+        try {
+            const {data} = await axios.get(url);
+            setTutorials(data) 
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getTutorials()
+    },[])
+
+    console.log(tutorials);
     return (
         <div>Home</div>
     )
