c978f1434ae62d3e8c13867c62494eabc78d643b
Erhan <erhantezer68@gmail.com>
2022-12-30 12:42:30 +0300

    edited

--- react/class-notes/rc12-13_router-example/src/App.js
+++ react/class-notes/rc12-13_router-example/src/App.js
@@ -1,12 +1,33 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import './App.css';
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+
+//* İc ice sayfalari gsotermek icin Nested Route kullanilabilir.
+
+//? Link, NavLink ve Navigate componentleri declerative routing
+//? gerceklestirmek icin kullanilir.
+//? Ornegin: Link ve NavLink Sayfada gorulebilen, kullanciyla
+//? bir etkilesim icerisinde bulunarak yonledirme yapılan bir
+//? componentlerdir. (Nav v.b)
+
+//? Navigate sayfada gorulmeyen ve programsal olarak bir linkin
+//? bir baska linke yonledirmesi icin kullanilir. (v5 -> Redirect)
+
+//* useNavigate() ise imperative routing icin elverislidir.
+//* Ornegin bir fonksiyon,event veye UseEffect icerisinde programsal
+//* olarak yonledirme yapmak icin kullanilabilir.
 
 function App() {
   return (
     <BrowserRouter >
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="instructors" element={<Instructors/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
     </BrowserRouter>
   );
