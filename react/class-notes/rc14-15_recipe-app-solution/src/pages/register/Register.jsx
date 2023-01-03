a7219f605196396e1f84c585ac866b7e6bd9abfa
Erhan <erhantezer68@gmail.com>
2023-01-03 13:44:02 +0300

    register finished

--- react/class-notes/rc14-15_recipe-app-solution/src/pages/register/Register.jsx
+++ react/class-notes/rc14-15_recipe-app-solution/src/pages/register/Register.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import underConst from '../../assets/const.png';
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <img style={{ width: '70%', margin: '6rem 8rem' }} src={underConst} alt="" />
+    </div>
+  );
+};
 
-export default Register
+export default Register;
