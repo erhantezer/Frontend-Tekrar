3fe45cafd40602c8b74659d56b1ad857b616bb56
Erhan <erhantezer68@gmail.com>
2023-01-03 19:36:20 +0300

    updated

--- react/class-notes/rc14-15_recipe-app-solution/src/pages/home/Home.jsx
+++ react/class-notes/rc14-15_recipe-app-solution/src/pages/home/Home.jsx
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
-import axios from "axios";
-import Header from '../../components/header/Header';
 import { HeaderText, HomeImg, ImgDiv } from './Home.style';
-import homeSvg from '../../assets/home.svg';
+import axios from 'axios';
+import { useState } from 'react';
+import Header from '../../components/header/Header';
 import Cards from '../../components/cards/Cards';
+import homeSvg from '../../assets/home.svg';
 
 const Home = () => {
   const mealType = ['Breakfast', 'Lunch', 'Dinner', 'Snack', 'Teatime'];
   const [query, setQuery] = useState('egg');
   const [selectedMeal, setSelectedMeal] = useState(mealType[0]);
-  const [recipes, setRecipes] = useState(null)
+  const [recipes, setRecipes] = useState(null);
 
   const APP_ID = process.env.REACT_APP_APP_ID;
   const APP_KEY = process.env.REACT_APP_APP_KEY;
-
-  // "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
@@ -22,16 +20,16 @@ const Home = () => {
     if (query) {
       try {
         const { data } = await axios.get(url);
-        console.log(data.hits)
-        setRecipes(data.hits)
+        setRecipes(data.hits);
       } catch (error) {
         console.log(error);
       }
-    }else{
-    alert("Please enter your meal!")}
-  }
-  
+    } else {
+      alert('Please Enter your meal');
+    }
+  };
 
+  // console.log(recipes);
   return (
     <div>
       <Header
@@ -40,20 +38,19 @@ const Home = () => {
         mealType={mealType}
         getData={getData}
       />
-
-    {!recipes && (
-      <ImgDiv>
-        <HomeImg src={homeSvg}/>
-      </ImgDiv>
-    )}
-
-      {recipes?.length === 0 && (
-        <HeaderText>Text Food Can Not Be Found</HeaderText>
+      {!recipes && (
+        <ImgDiv>
+          <HomeImg src={homeSvg} />
+        </ImgDiv>
       )}
 
-      {recipes?.length > 0 && <Cards recipes={recipes}/>} 
-    </div>
-  )
-}
+      {recipes?.length === 0 && (
+        <HeaderText>The Food can not be found</HeaderText>
+      )}
 
-export default Home
+      {recipes?.length > 0 && <Cards recipes={recipes} />}
+    </div>
+  );
+};
+
+export default Home;
--- react/class-notes/rc14-15_recipe-app-solution/src/router/PrivateRouter.jsx
+++ react/class-notes/rc14-15_recipe-app-solution/src/router/PrivateRouter.jsx
@@ -3,9 +3,9 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
   const currentUser =JSON.parse(sessionStorage.getItem("user"))
-  console.log(currentUser.username)
+  
   return (
-    currentUser.username ? <Outlet/> : <Navigate to="/login"/>
+    currentUser ? <Outlet/> : <Navigate to="/login"/>
   )
 }
 
