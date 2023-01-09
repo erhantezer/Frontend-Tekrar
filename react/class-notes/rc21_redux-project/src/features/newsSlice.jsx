b9eb02da305007410c899bd863096c7c9c674428
Erhan <erhantezer68@gmail.com>
2023-01-10 02:03:31 +0300

    comment added

--- react/class-notes/rc21_redux-project/src/features/newsSlice.jsx
+++ react/class-notes/rc21_redux-project/src/features/newsSlice.jsx
@@ -1,17 +1,33 @@
 //! rxslice
 
-// import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-// const initialState = {
+const initialState = {
+    newsList: [],
+    loading: false,
+    error: false,
+};
 
-// }
+//? State'lerin API gibi async kaynaklardan gelen verilere gore guncellenmesi gerekebilir.
+//? Ancak boyle bir durumda async islem tamamlandiktan sonra state guncellenmelidir.
+//? Gonderilen api istegi ile dogrudan state guncellememelidir.
+//? Islemin tamamlanmasi ile gelen veriye gore state'in guncellenemsini saglamak
+//? adina bir arabirim kullanilmaktadir.
+//? Bu arabirim middleware denilir.Redux-Toolkit, default olarak Thunk kullanmaktadir.
+//! Thunk'ın amaci reducers'a islenmis sonuclari gondermeden once gecikmeli asenkron 
+//! islemlerinin yurutulmesini saglamaktir.
 
-// const newsSlice = createSlice({
-//   name: second,
-//   initialState,
-//   reducers: {}
-// });
 
-// export const {} = newsSlice.actions
 
-// export default newsSlice.reducer
+
+
+
+const newsSlice = createSlice({
+  name: "news",
+  initialState,
+  reducers: {}
+});
+
+export const {} = newsSlice.actions
+
+export default newsSlice.reducer
