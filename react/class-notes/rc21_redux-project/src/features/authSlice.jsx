c480fb386b3f0c3f78759c3a6c06027be8914471
Erhan <erhantezer68@gmail.com>
2023-01-10 01:04:21 +0300

    redux project started

---
+++ react/class-notes/rc21_redux-project/src/features/authSlice.jsx
@@ -0,0 +1,17 @@
+//! rxslice
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const authSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = authSlice.actions
+
+export default authSlice.reducer
