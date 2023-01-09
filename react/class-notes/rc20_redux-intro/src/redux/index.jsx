efe7d1adb3197a2f780d8920a02cd3a264dddd3a
Erhan <erhantezer68@gmail.com>
2023-01-10 00:57:55 +0300

    updated

--- react/class-notes/rc20_redux-intro/src/redux/index.jsx
+++ react/class-notes/rc20_redux-intro/src/redux/index.jsx
@@ -1,12 +1,12 @@
 
 import { legacy_createStore as createStore, combineReducers } from "redux";
 import counterReducer from "./reducers/counterReducer";
-// import todoReducer from "./reducers/todoReducer";
+import todoReducer from "./reducers/todoReducer";
 
 
 const rootReducer = combineReducers({
   count: counterReducer,
-//   todo: todoReducer,
+  todo: todoReducer,
 });
 
 export const store = createStore(rootReducer);
