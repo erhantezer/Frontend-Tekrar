cf15155c7bceaf38af7ba95126f6c7786b25682a
Erhan <erhantezer68@gmail.com>
2023-01-11 02:34:46 +0300

    useMemo

--- react/class-notes/rc22_useref-memo-callback/src/App.js
+++ react/class-notes/rc22_useref-memo-callback/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import HeaderMemo from "./components/HeaderMemo";
@@ -7,41 +7,78 @@ function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState("")
   const [search, setSearch] = useState("")
-  const [data, setData] = useState([])
+  const [user, setUser] = useState([])
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        setData(data)
+        setUser(data)
       })
   }, [])
 
+  // const filteredData = user?.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+// console.log(filteredData)
+const filteredData = useMemo(()=>{
+  console.log("useMemo çalıştı")
+  return user?.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()));
+  },[user,search])
+
+const handleClick = () => {
+  setSearch(text)
+}
 
 
   return (
     <div className="container mt-2">
       <div>
         <Header />
-        <hr />
+        
         <HeaderMemo />
       </div>
-      <hr />
+      
       <div>
         <p>{count}</p>
-        <button className='btn btn-danger' onClick={() => setCount(count + 1)}>Increment</button>
+        <button className='btn btn-danger' onClick={() => setCount(count + 1)}>
+        Increment
+        </button>
       </div>
-      <hr />
+    
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <input type='text' />
-        <button type='button'>Search</button>
+        <input 
+        type='text' 
+        value={text}
+        //! onChange={({target}) => setText(target.value)}
+        onChange={(e) => setText(e.target.value)}
+        />
+        <button type='button' onClick={handleClick}>Search</button>
       </div>
       <div className="row">
-        <Card data={data} />
+        <Card data={filteredData} />
       </div>
+      
+      <h1>useRef</h1>
     </div>
   );
 }
 
 export default App;
+
+// stateler her değiştiğinde component render olacağı için alt componentler de 
+// yeniden oluşturulur. bunu önlemenin yolu react memo kullanmak. memo bize sadece 
+// ilgili componente gönderilen değer değiştiğinde sardığı componenti tekrar render ettirir. 
+// faydasız kaldığı yer object tipli verilerdir. onu engellemenin yolu da useMemo kullanmak 
+
+//useMemo
+// Shallow comparison’da eğer karşılaştırılan tipler nesne (Object) ise içerisindeki
+ // değerleri değil referans değerleri karşılaştırılır. Eğer karşılaştırılan nesneler
+ // memory’de aynı adresi gösteriyorsa true göstermiyorsa false olarak değer döndürür.
+
+// Input alanına bir değer girdiğimiz zaman App.js tekrar render edildiği için 
+// filteredData tekrar oluşur. filteredData tekrardan oluştuğu için Card componentine 
+// göndermiş olduğumuz data her seferinde farklı bir adrese sahip olur. Bu yüzdende 
+// React.memo Card componentine ilk seferde göndermiş olduğumuz data array’inin tutulduğu 
+// adres ile render edildikten sonra gelen data array’inin farklı adreste bulunduğunu gördüğü 
+// için CArd componentini tekrar render eder. Props olarak verdiğimiz array değişmediği halde 
+// Card componentinin render edilmesini engelleyebilmek için useMemo kullanabiliriz.
--- react/class-notes/rc22_useref-memo-callback/src/components/Card.jsx
+++ react/class-notes/rc22_useref-memo-callback/src/components/Card.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import {memo} from 'react'
 
 const Card = ({ data }) => {
+    console.log("Render => Card components")
     return (
         <div className="row row-cols-1 row-cols-md-4 g-4 p-2">
             {
                 data?.map(item => {
                     return (
                         <div className="col" key={item.id}>
-                            <div className="card" style={{ minHeight: "400px", maxHeight: "400px" }}>
-                                <img src={`https://avatars.dicebear.com/v2/avataaars/${item.id}.svg`} className="card-img-top" alt="..." />
+                            <div className="card" >
+                                <img src={`https://avatars.dicebear.com/v2/avataaars/${item.id}.svg`}  className="card-img-top" alt="..." />
                                 <div className="card-body">
                                     <h5 className="card-title">{item.name}</h5>
                                     <p className="card-text">
@@ -24,4 +25,4 @@ const Card = ({ data }) => {
     )
 }
 
-export default Card
+export default memo(Card)
